9d4ce95f5d3d600eaf6abde35d37861e57145bf0
Manda Paul Pramod <20481A05E0@gecgudlavallerumic.in>
2023-06-21 12:01:22 +0530

    first commit

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.get('/trains', async (req, res) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
